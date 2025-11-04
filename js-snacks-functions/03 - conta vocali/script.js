b36295139169f49ab5df5f1dfb89e3247b975485
andrea-desantis <andrea.desantis290@gmail.com>
2025-11-04 14:50:00 +0100

    esercizio 3

--- js-snacks-functions/03 - conta vocali/script.js
+++ js-snacks-functions/03 - conta vocali/script.js
@@ -1,13 +1,24 @@
-/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
+/* Scrivi una funzione che accetti una stringa e restituisca 
+   il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function numVocali(word){
+    let numero=0;
 
+    for(let i=0; i<word.length; i++){
+        
+        if(word[i] === ("a") || word[i] === ("e") || word[i] === ("i") || word[i] === ("o") || word[i] === ("u")){
+            numero++;
+        }
+    }
+    return numero;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(numVocali(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
