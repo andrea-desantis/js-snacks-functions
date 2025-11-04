893a7ccb0fd5d683d8649870eebd6cc4e93f5d8b
andrea-desantis <andrea.desantis290@gmail.com>
2025-11-04 14:55:10 +0100

    esercizio 4

--- js-snacks-functions/04 - saluti/script.js
+++ js-snacks-functions/04 - saluti/script.js
@@ -1,13 +1,15 @@
-/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+/* Scrivi una funzione che accetti una stringa contenente un nome 
+   e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
 const name = 'Mario';
 
 
 // Dichiara la funzione qui.
 
+const ciao = (name) => `ciao ${name}`;
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(ciao(name));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
