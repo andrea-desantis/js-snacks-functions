432ed0e9c0429354449a7ed1e901a2f447ce9f8f
andrea-desantis <andrea.desantis290@gmail.com>
2025-11-04 14:04:14 +0100

    esercizio 1

--- js-snacks-functions/01 - Iniziali/script.js
+++ js-snacks-functions/01 - Iniziali/script.js
@@ -4,10 +4,16 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function iniziali(names){
+    let primaLet= [];
+    for(let i=0; i<names.length; i++){
+        primaLet [i] = names[i].at(0); 
+    }
+    return primaLet;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(iniziali(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
