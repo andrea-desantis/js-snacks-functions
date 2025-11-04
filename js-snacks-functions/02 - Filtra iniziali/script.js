1c440bbd6c12e1565007ec72afbee8b5b8022174
andrea-desantis <andrea.desantis290@gmail.com>
2025-11-04 14:11:32 +0100

    esercizio 2

--- js-snacks-functions/02 - Filtra iniziali/script.js
+++ js-snacks-functions/02 - Filtra iniziali/script.js
@@ -1,13 +1,26 @@
-/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
+/* Scrivi una funzione che accetti un'array di stringhe e una lettera e 
+restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function nomeConLet(names, lettera){
+    let nomiConIniziale=[];
+    let j=0;
+    for(let i=0; i<names.length; i++){
+        
+        if(names[i].at(0) === lettera){
+            nomiConIniziale [j] = names[i];
+            j++;
+        }
 
+    }
+    return nomiConIniziale;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(nomeConLet(names, "A"));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
